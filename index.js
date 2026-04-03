efdc5afc657d00358786f1ffeb10dfb7756594fa
truongdok7-png <truongdok7@gmail.com>
2026-04-03 08:18:30 +0700

    Remove Discord bot implementation from index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
-});
-
-client.on("ready", () => {
-  console.log(`Bot online: ${client.user.tag}`);
-});
-
-client.on("messageCreate", (message) => {
-  if (message.content === "!ping") {
-    message.reply("pong 🏓");
-  }
-});
-
-client.login(process.env.TOKEN);
